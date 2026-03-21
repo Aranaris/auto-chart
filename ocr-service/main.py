75fa055f84a3b4bac729c76cff5f69d2e93e3be2
aranaris <vinceye.cm@gmail.com>
2026-03-20 17:35:00 -0700

    add requirements.txt and filepath fixes

--- ocr-service/main.py
+++ ocr-service/main.py
@@ -3,13 +3,22 @@ from fastapi import FastAPI, Request, BackgroundTasks
 from docling.document_converter import DocumentConverter
 from docling.datamodel.base_models import InputFormat
 from docling.datamodel.pipeline_options import PdfPipelineOptions, OcrMacOptions
+from dotenv import load_dotenv
+
+# Load variables from .env into the system environment
+load_dotenv()
+
+# Use the variables with fallbacks (defaults)
+INBOUND_DIR = os.getenv("INBOUND_DIR", "./data/inbound")
+OUTBOUND_DIR = os.getenv("OUTBOUND_DIR", "./data/processing")
+APP_PORT = int(os.getenv("APP_PORT", 8000))
 
 app = FastAPI()
 
 # Use Apple's native Vision framework instead of Tesseract
 pipeline_options = PdfPipelineOptions()
 pipeline_options.do_ocr = True
-pipeline_options.ocr_options = OcrMacOptions() # This is the "M1 Turbo" button
+pipeline_options.ocr_options = OcrMacOptions()  # This is the "M1 Turbo" button
 
 converter = DocumentConverter()
 
@@ -18,15 +27,17 @@ def process_document(file_path: str, file_name: str):
     try:
         # Convert TIFF to Docling's internal structured representation
         result = converter.convert(file_path)
-        
+
         # Export to Markdown (best for LLMs/Processing) or JSON (best for mapping)
         content_md = result.document.export_to_markdown()
-        
+
         # Save the result to your 'processing' folder
-        output_path = f"./srv/sftpgo/processing/{file_name}.md"
+        file_root, _ = os.path.splitext(file_name)
+        outbound_file_name = f"{file_root}.md"
+        output_path = os.path.join(OUTBOUND_DIR, outbound_file_name)
         with open(output_path, "w") as f:
             f.write(content_md)
-            
+
         print(f"✅ Conversion complete: {output_path}")
         # NEXT STEP: Trigger the FHIR Mapper
     except Exception as e:
@@ -36,17 +47,17 @@ def process_document(file_path: str, file_name: str):
 async def handle_event(request: Request, background_tasks: BackgroundTasks):
     try:
         data = await request.json()
-        print(f"📥 Received Webhook Data: {data}") # Debug: See what SFTPGo is sending
+        print(f"📥 Received Webhook Data: {data}")  # Debug: See what SFTPGo is sending
         
-        file_path = data.get("fs_path")
         file_name = data.get("object_name")
+        file_path = os.path.join(INBOUND_DIR, file_name)
 
         # CRITICAL: Ensure the directory exists before starting
-        os.makedirs("./srv/sftpgo/processing", exist_ok=True)
+        os.makedirs(f"{OUTBOUND_DIR}", exist_ok=True)
 
         background_tasks.add_task(process_document, file_path, file_name)
         return {"status": "queued"}
-        
+
     except Exception as e:
         print(f"❌ Webhook Handler Failed: {str(e)}")
         return {"status": "error", "message": str(e)}
